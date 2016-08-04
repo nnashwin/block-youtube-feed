e02abc8759619c33b53e1d5e3788e42631311d41
ttymed <tyler.boright@gmail.com>
2016-08-04 11:42:32 +0800

    changed the manifest to work on different pages and am now blocking recommended videos on the different pages.

--- remove-recommendations.js
+++ remove-recommendations.js
@@ -1,7 +1,18 @@
 'use strict'
 var pageContainer = document.getElementById('feed-main-what_to_watch')
-pageContainer.remove()
+if (pageContainer) {
+    pageContainer.remove()
+}
 
 var brandedContent = document.getElementById('branded-page-v2-secondary-col')
-brandedContent.remove()
+if (brandedContent) {
+    brandedContent.remove()
+}
+
+var recommendedVideos = document.getElementById("watch7-sidebar-modules")
+if (recommendedVideos) {
+    recommendedVideos.remove()
+}
+
+console.log('tyler')
 
